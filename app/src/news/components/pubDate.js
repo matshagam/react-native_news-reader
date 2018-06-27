294e1eb49c207d97671bbcaa69e8b4109046d04c
Dimitri A. Matveev <dimitri.matveev@icloud.com>
2018-06-27 20:41:38 +0300

    Update pubDate.js

--- app/src/news/components/pubDate.js
+++ app/src/news/components/pubDate.js
@@ -1,23 +1,20 @@
-import React, { PureComponent } from 'react';
-import { Text, StyleSheet } from 'react-native';
-import formatDate from 'date-fns/format';
-import ruLocale from 'date-fns/locale/ru';
+import React, { PureComponent } from 'react'
+import { Text, StyleSheet } from 'react-native'
+import formatDate from 'date-fns/format'
+import ruLocale from 'date-fns/locale/ru'
 
-export default class PubDate extends PureComponent {
-  render() {
-    const { data } = this.props;
-    return (
-      <Text style={styles.pubDate}>
-        {formatDate(data, 'DD MMMM, YYYY', { locale: ruLocale })}
-      </Text>
-    );
-  }
+export default function PubDate(props) {
+  return (
+    <Text style={styles.pubDate}>
+      {formatDate(props.pubDate, 'DD MMMM, YYYY', { locale: ruLocale })}
+    </Text>
+  )
 }
 
 const styles = StyleSheet.create({
   pubDate: {
     color: 'rgba(128, 128, 128, 0.7)',
     fontSize: 12,
-    paddingBottom: 6
-  }
-});
+    paddingBottom: 6,
+  },
+})
