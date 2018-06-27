2784a020bee58bcdc5c606de021f4b27ba25722b
Dimitri A. Matveev <dimitri.matveev@icloud.com>
2018-06-27 19:29:21 +0300

    Update searchInput.js

--- app/src/news/components/searchInput.js
+++ app/src/news/components/searchInput.js
@@ -1,53 +1,45 @@
-import React, { PureComponent } from 'react';
-import { View, TextInput, StyleSheet } from 'react-native';
-import { Icon } from 'react-native-elements';
+import React, { PureComponent } from 'react'
+import { View, TextInput, StyleSheet } from 'react-native'
+import { Icon } from 'react-native-elements'
 
-export default class SearchInput extends PureComponent {
-  render() {
-    const { query, isHiddenSearch } = this.props.searchInput;
-    if (!isHiddenSearch) {
-      return (
-        <View style={styles.textInputContainer}>
-          <TextInput
-            onChangeText={text =>
-              this.props.handleKeySearch(text.toLowerCase())
-            }
-            style={styles.searchInput}
-            placeholder="Начните вводить слово..."
-            value={query}
-            selectionColor="grey"
-            numberOfLines={1}
-            maxLength={24}
-            autoCorrect={false}
-            autoFocus={true}
-            onKeyPress={event =>
-              event.nativeEvent.key === 'Backspace' && query.length === 2
-                ? this.props.clearQuery()
-                : null
-            }
-          />
-          <Icon
-            containerStyle={styles.iconSearchContainer}
-            iconStyle={styles.icon}
-            size={20}
-            underlayColor="transparent"
-            type="ionicon"
-            name="ios-close-circle-outline"
-            onPress={this.props.clearQuery}
-          />
-        </View>
-      );
-    } else {
-      return null;
-    }
-  }
+export default function SearchInput(props) {
+  return !props.searchInput.isHiddenSearch ? (
+    <View style={styles.textInputContainer}>
+      <TextInput
+        onChangeText={text => props.handleKeySearch(text.toLowerCase())}
+        style={styles.searchInput}
+        placeholder="Начните вводить слово..."
+        value={props.searchInput.query}
+        selectionColor="grey"
+        numberOfLines={1}
+        maxLength={24}
+        autoCorrect={false}
+        autoFocus={true}
+        onKeyPress={event =>
+          event.nativeEvent.key === 'Backspace' &&
+          props.searchInput.query.length === 2
+            ? props.clearQuery()
+            : null
+        }
+      />
+      <Icon
+        containerStyle={styles.iconSearchContainer}
+        iconStyle={styles.icon}
+        size={20}
+        underlayColor="transparent"
+        type="ionicon"
+        name="ios-close-circle-outline"
+        onPress={props.clearQuery}
+      />
+    </View>
+  ) : null
 }
 
 const styles = StyleSheet.create({
   textInputContainer: {
     height: 46,
     justifyContent: 'center',
-    backgroundColor: '#f5f5f5'
+    backgroundColor: '#f5f5f5',
   },
   searchInput: {
     fontSize: 16,
@@ -58,19 +50,19 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 50,
     borderBottomRightRadius: 50,
     borderBottomLeftRadius: 50,
-    backgroundColor: '#eaeaea'
+    backgroundColor: '#eaeaea',
   },
   iconSearchContainer: {
     top: 13,
     right: 16,
-    position: 'absolute'
+    position: 'absolute',
   },
   textInputContainer: {
     height: 46,
     justifyContent: 'center',
-    backgroundColor: '#f5f5f5'
+    backgroundColor: '#f5f5f5',
   },
   icon: {
-    color: 'rgba(128, 128, 128, 0.4)'
-  }
-});
+    color: 'rgba(128, 128, 128, 0.4)',
+  },
+})
