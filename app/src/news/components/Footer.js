1f25f84ae6a79500cf4734207daeac2e790255c2
Dimitri A. Matveev <dimitri.matveev@icloud.com>
2018-06-27 19:10:51 +0300

    Update Footer.js

--- app/src/news/components/Footer.js
+++ app/src/news/components/Footer.js
@@ -1,62 +1,56 @@
-import React, { Component } from "react";
-import { View, StyleSheet } from "react-native";
-import { Icon } from "react-native-elements";
+import React, { PureComponent } from 'react'
+import { View, StyleSheet } from 'react-native'
+import { Icon } from 'react-native-elements'
 
-export default class Footer extends Component {
-  render() {
-    const isHiddenSearch = this.props.isHiddenSearch,
-      isHiddenFooter = this.props.isHiddenHeaderFooter,
-      modalVisible = this.props.modalVisible;
-
-    return !isHiddenFooter ? (
-      <View style={styles.iconsContainer}>
-        <Icon
-          iconStyle={styles.icon}
-          size={45}
-          underlayColor="transparent"
-          type="ionicon"
-          name="ios-search-outline"
-          onPress={() => {
-            this.props.updatePostsState({
-              isHiddenSearch: !isHiddenSearch
-            });
-          }}
-        />
-        <Icon
-          iconStyle={styles.icon}
-          size={45}
-          underlayColor="transparent"
-          type="ionicon"
-          name="ios-settings-outline"
-          onPress={() => {
-            this.props.updatePostsState({
-              modalVisible: !modalVisible
-            });
-          }}
-        />
-        <Icon
-          iconStyle={styles.icon}
-          size={45}
-          underlayColor="transparent"
-          type="ionicon"
-          name="ios-arrow-dropup-outline"
-          onPress={this.props.scrollToTop}
-        />
-      </View>
-    ) : null;
-  }
+export default function Footer(props) {
+  return !props.isHiddenHeaderFooter ? (
+    <View style={styles.iconsContainer}>
+      <Icon
+        iconStyle={styles.icon}
+        size={45}
+        underlayColor="transparent"
+        type="ionicon"
+        name="ios-search-outline"
+        onPress={() => {
+          props.updatePostsState({
+            isHiddenSearch: !props.isHiddenSearch,
+          })
+        }}
+      />
+      <Icon
+        iconStyle={styles.icon}
+        size={45}
+        underlayColor="transparent"
+        type="ionicon"
+        name="ios-settings-outline"
+        onPress={() => {
+          props.updatePostsState({
+            modalVisible: !props.modalVisible,
+          })
+        }}
+      />
+      <Icon
+        iconStyle={styles.icon}
+        size={45}
+        underlayColor="transparent"
+        type="ionicon"
+        name="ios-arrow-dropup-outline"
+        onPress={props.scrollToTop}
+      />
+    </View>
+  ) : null
 }
 
 const styles = StyleSheet.create({
   icon: {
-    color: "rgba(128, 128, 128, 0.6)"
+    color: 'rgba(128, 128, 128, 0.6)',
   },
   iconsContainer: {
-    justifyContent: "space-between",
-    alignSelf: "center",
-    position: "absolute",
-    flexDirection: "row",
+    justifyContent: 'space-between',
+    alignSelf: 'center',
+    position: 'absolute',
+    flexDirection: 'row',
     bottom: 0,
-    width: 170
-  }
-});
+    width: 170,
+  },
+})
