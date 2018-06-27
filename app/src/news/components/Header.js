6b132b995d46fd9d6051f0dbbc0a9b61daa16a32
Dimitri A. Matveev <dimitri.matveev@icloud.com>
2018-06-27 19:11:10 +0300

    Update Header.js

--- app/src/news/components/Header.js
+++ app/src/news/components/Header.js
@@ -1,26 +1,22 @@
-import React, { Component } from "react";
-import { Text, StyleSheet, View } from "react-native";
+import React, { Component } from 'react'
+import { Text, StyleSheet, View } from 'react-native'
 
-export default class Header extends Component {
-  render() {
-    const isHiddenHeader = this.props.isHiddenHeaderFooter;
-
-    return !isHiddenHeader ? (
-      <View style={styles.header}>
-        <Text style={styles.textHeader}>НОВОСТИ</Text>
-      </View>
-    ) : null;
-  }
+export default function Header(props) {
+  return !props.isHiddenHeaderFooter ? (
+    <View style={styles.header}>
+      <Text style={styles.textHeader}>НОВОСТИ</Text>
+    </View>
+  ) : null
 }
 
 const styles = StyleSheet.create({
   header: {
     height: 44,
-    justifyContent: "center"
+    justifyContent: 'center',
   },
   textHeader: {
-    fontWeight: "bold",
+    fontWeight: 'bold',
     fontSize: 22,
-    paddingLeft: 16
-  }
-});
+    paddingLeft: 16,
+  },
+})
