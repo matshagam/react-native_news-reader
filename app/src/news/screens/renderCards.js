92eda86cb930b7429bd51e3d1149530fe27e909e
Dimitri A. Matveev <dimitri.matveev@icloud.com>
2018-06-27 20:39:09 +0300

    Update renderCards.js

--- app/src/news/screens/renderCards.js
+++ app/src/news/screens/renderCards.js
@@ -1,211 +1,221 @@
-import React, { Component } from "react";
-import { FlatList, View, StyleSheet } from "react-native";
-import formatDate from "date-fns/format";
+import React, { Component } from 'react'
+import { FlatList, View, StyleSheet } from 'react-native'
+import formatDate from 'date-fns/format'
 
 import {
   setAsync,
   getAsync,
   MY_MAC_SERVER,
   fetchDATA,
-  filterData
-} from "../config/helpers";
+  filterData,
+} from '../config/helpers'
 
-import Card from "../container/Card";
-import SettingsModal from "./settingsModal";
-import SearchInput from "../components/searchInput";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Card from '../container/Card'
+import SettingsModal from './settingsModal'
+import SearchInput from '../components/searchInput'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 export default class RenderCards extends Component {
   static navigationOptions = {
-    header: null
+    header: null,
     /* headerTitle: "НОВОСТИ",
     headerTitleStyle: {
-    position: "absolute",
-    left: 0,
-    fontSize: 19,
-    fontWeight: "bold"
-     },
-    headerTruncatedBackTitle: `НОВОСТИ`,
+      position: "absolute",
+      left: 0,
+      fontSize: 19,
+      fontWeight: "bold"
+    },
+    headerTruncatedBackTitle: `НОВОСТИ`
     headerStyle: {
-       elevation: 0,
-       borderBottomWidth: 0,
-       borderBottomColor: "#fff",
-       backgroundColor: "#f5f5f5"
-     }*/
-  };
+      elevation: 0,
+      borderBottomWidth: 0,
+      borderBottomColor: "#fff",
+      backgroundColor: "#f5f5f5"
+    } */
+  }
 
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
       news: [],
       page: 1,
       loading: false,
-      query: "",
+      query: '',
       isHiddenSearch: true,
       isHiddenHeaderFooter: false,
       modalVisible: false,
       allDate: false,
-      today: formatDate(new Date(), "YYYY-MM-DD"),
+      today: formatDate(new Date(), 'YYYY-MM-DD'),
       todayNews: false,
       popularSorting: false,
-      favoritesSorting: false
-    };
-    this.updatePostsState = this.updatePostsState.bind(this);
-    this.handleKeySearch = this.handleKeySearch.bind(this);
+      favoritesSorting: false,
+    }
+    this.updatePostsState = this.updatePostsState.bind(this)
+    this.handleKeySearch = this.handleKeySearch.bind(this)
   }
 
   componentDidMount() {
-    this.getData();
+    this.getData()
   }
 
   getData = () => {
-    console.log("get data...");
+    console.log('get data...')
 
-    const { page, query, news, isHiddenSearch, today, todayNews } = this.state;
+    const { page, query, news, isHiddenSearch, today, todayNews } = this.state
     this.setState({
-      loading: true
-    });
+      loading: true,
+    })
 
-    if (isHiddenSearch && query == "") {
+    if (isHiddenSearch && query === '') {
       fetchDATA(
         !todayNews
-          ? MY_MAC_SERVER + `?_limit=3&_page=${page}&_sort=pubDate&_order=desc`
-          : MY_MAC_SERVER + `?_limit=3&_page=${page}&pubDate=${today}`
+          ? `${MY_MAC_SERVER}?_limit=3&_page=${page}&_sort=pubDate&_order=desc`
+          : `${MY_MAC_SERVER}?_limit=3&_page=${page}&pubDate=${today}`,
       )
         .then(res => res.map(filterData => filterData))
         .then(data => {
           this.setState({
             news: page === 1 ? data : [...news, ...data],
-            loading: false
-          });
+            loading: false,
+          })
           console.log(
             data.length > 3
-              ? "ПРЕВЫШЕН _limit: " + data.length + " > " + "3"
-              : "В лимите..."
-          );
-          if (!todayNews) setAsync("prevStateNews", this.state.news);
-        });
+              ? `ПРЕВЫШЕН _limit: ${data.length} > ` + '3'
+              : 'В лимите...',
+          )
+          if (!todayNews) setAsync('prevStateNews', news)
+        })
     }
 
-    if (!isHiddenSearch && query !== "") {
-      fetchDATA(MY_MAC_SERVER + `?_sort=pubDate&_order=desc&q=${query}`)
+    if (!isHiddenSearch && query !== '') {
+      fetchDATA(`${MY_MAC_SERVER}?_sort=pubDate&_order=desc&q=${query}`)
         .then(res => res.map(filterData => filterData))
         .then(data => {
           this.setState({
             news: data,
             loading: false,
-            page: 1
-          });
-        });
+            page: 1,
+          })
+        })
     }
 
-    if (!isHiddenSearch && query == "") {
-      console.log("load data from storage... ");
+    if (!isHiddenSearch && query === '') {
+      console.log('load data from storage... ')
 
-      getAsync("prevStateNews").then(data => {
+      getAsync('prevStateNews').then(data => {
         this.setState({
-          news: data == news ? news : data
-        });
-      });
+          news: data === news ? news : data,
+        })
+      })
     }
-  };
-
-  handleKeySearch(text) {
-    this.setState(
-      {
-        query: text
-      },
-      () => {
-        this.state.query.length >= 3 ? this.getData() : null;
-      }
-    );
   }
 
   clearQuery = () => {
     this.setState(
       {
-        query: ""
+        query: '',
       },
       () => {
-        this.getData();
-      }
-    );
-  };
+        this.getData()
+      },
+    )
+  }
 
   updatePostsState(data) {
-    this.setState(data);
+    this.setState(data)
   }
 
   updateStateSettings = () => {
+    const modalVisible = this.state.modalVisible
     this.setState(
       {
-        modalVisible: !this.state.modalVisible
+        modalVisible: !modalVisible,
       },
       () => {
-        this.getData();
-      }
-    );
-  };
+        this.getData()
+      },
+    )
+  }
 
   scrollToTop = () => {
+    const isHiddenHeaderFooter = this.state.isHiddenHeaderFooter
     this.refs.listRef.scrollToOffset({
       x: 0,
       y: 0,
-      animated: true
-    });
-    !this.state.isHiddenHeaderFooter
+      animated: true,
+    })
+    !isHiddenHeaderFooter
       ? this.setState({
-          isHiddenHeaderFooter: !this.state.isHiddenHeaderFooter
+          isHiddenHeaderFooter: !isHiddenHeaderFooter,
         })
-      : null;
-  };
+      : null
+  }
+
+  handleKeySearch(text) {
+    const query = this.state.query.length
+
+    this.setState(
+      {
+        query: text,
+      },
+      () => {
+        query >= 3 ? this.getData() : null
+      },
+    )
+  }
 
   scrollEvent(event) {
-    const { isHiddenHeaderFooter, isHiddenSearch } = this.state;
-    let scrollLength = event.nativeEvent.contentOffset.y;
+    const { isHiddenHeaderFooter, isHiddenSearch } = this.state
+    const scrollLength = event.nativeEvent.contentOffset.y
 
     scrollLength > 100 && !isHiddenSearch
       ? this.setState({
-          isHiddenSearch: !isHiddenSearch
+          isHiddenSearch: !isHiddenSearch,
         })
-      : null;
+      : null
 
     scrollLength > 44 && !isHiddenHeaderFooter
       ? this.setState({
-          isHiddenHeaderFooter: !isHiddenHeaderFooter
+          isHiddenHeaderFooter: !isHiddenHeaderFooter,
         })
-      : null;
+      : null
   }
 
   onEndReached = () => {
     this.setState(
       {
-        page: this.state.page + 1
+        page: this.state.page + 1,
       },
-      () => this.getData()
-    );
-  };
+      () => this.getData(),
+    )
+  }
 
   renderItem = ({ item }) => {
-    const { navigation } = this.props;
-    return <Card data={item} navigation={navigation} />;
-  };
+    const { navigation } = this.props
+    return <Card data={item} navigation={navigation} />
+  }
 
   render() {
     const {
       news,
       isHiddenHeaderFooter,
       isHiddenSearch,
-      modalVisible
-    } = this.state;
+      modalVisible,
+      query,
+      allDate,
+      todayNews,
+      popularSorting,
+      favoritesSorting,
+    } = this.state
 
     return (
       <View style={styles.container}>
         <Header isHiddenHeaderFooter={isHiddenHeaderFooter} />
         <SearchInput
-          searchInput={this.state}
+          query={query}
+          isHiddenSearch={isHiddenSearch}
           clearQuery={this.clearQuery}
           handleKeySearch={this.handleKeySearch}
         />
@@ -215,15 +225,24 @@ export default class RenderCards extends Component {
           keyExtractor={(item, index) => index.toString()}
           ref="listRef"
           onMomentumScrollBegin={event => {
-            this.scrollEvent(event);
+            this.scrollEvent(event)
           }}
           onMomentumScrollEnd={() =>
             this.setState({
-              isHiddenHeaderFooter: !isHiddenHeaderFooter
+              isHiddenHeaderFooter: !isHiddenHeaderFooter,
             })
           }
           onEndReached={this.onEndReached}
           onEndReachedThreshold={0.5}
+        />
+        <SettingsModal
+          modalVisible={modalVisible}
+          allDate={allDate}
+          todayNews={todayNews}
+          popularSorting={popularSorting}
+          favoritesSorting={favoritesSorting}
+          updatePostsState={this.updatePostsState}
+          updateStateSettings={this.updateStateSettings}
         />
         <Footer
           isHiddenSearch={isHiddenSearch}
@@ -232,20 +251,15 @@ export default class RenderCards extends Component {
           updatePostsState={this.updatePostsState}
           scrollToTop={this.scrollToTop}
         />
-        <SettingsModal
-          settings={this.state}
-          updatePostsState={this.updatePostsState}
-          updateStateSettings={this.updateStateSettings}
-        />
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     paddingTop: 20,
-    backgroundColor: "#f5f5f5",
-    flex: 1
-  }
-});
+    backgroundColor: '#f5f5f5',
+    flex: 1,
+  },
+})
